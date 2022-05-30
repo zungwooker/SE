5ae6a3e8a4a4bfa16b074e507cb8ef8acc49d72c
zungwooker <zungwooker@gmail.com>
2022-05-30 18:51:25 +0900

    system 1

---
+++ 정욱code/CtrlMemberManagement.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class CtrlMemberManagement
+{
+private:
+    /* data */
+public:
+    CtrlMemberManagement(/* args */);
+    ~CtrlMemberManagement();
+};
+
+CtrlMemberManagement::CtrlMemberManagement(/* args */)
+{
+}
+
+CtrlMemberManagement::~CtrlMemberManagement()
+{
+}
